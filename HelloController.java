f9249eba73b833028a08782401a0cc4dfb9b20a4
Nguyen Duc Anh <137867811+ducanh0@users.noreply.github.com>
2023-12-10 21:28:14 +0700

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,14 @@
+package main.oop_ui_2324;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
